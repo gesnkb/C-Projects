168f4183aa1ad25627a4d524d9e8d112e9f01204
gesnkb <54376836+gesnkb@users.noreply.github.com>
2020-12-11 21:13:06 -0700

    Add files via upload

---
+++ Pets/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Cat : Pet
+    {
+
+        public Cat( string name, string owner, double weight) : base("cat", name, owner, weight)
+        {
+            
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+        }
+        public string meow(int count)
+        {
+            string result = "";
+            for (int i = 0; i <= count - 1; i++)
+            {
+                result += "meow.\t";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Pets/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Dog: Pet
+    {
+       
+        public Dog(string name, string owner, double weight):base("dog", name, owner, weight)
+        {
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+
+        }
+        public string bark(int count)
+        {
+            string result = "";
+            for (int i = 0; i <= count - 1; i++)
+            {
+                result += "bark!\t";
+            }
+
+            return result;
+        }
+    }
+}
